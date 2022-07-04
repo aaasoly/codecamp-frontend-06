6f298ce7acd518599e4067dbd17fa8c38d811cfb
aaasoly <imnamesol@gmail.com>
2022-07-04 22:59:48 +0900

    :lipstick: unsplash api UI frame
     unsplash api UI frame

--- freeboard_frontend/pages/boards/api/index.tsx
+++ freeboard_frontend/pages/boards/api/index.tsx
@@ -1,15 +1,96 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-// import { createApi } from "unsplash-js";
-// import nodeFetch from "node-fetch";
+import styled from "@emotion/styled";
 
-export default function OpenapiWithUseEffectPage() {
+const Wrapper = styled.div`
+  width: 1600px;
+  height: 900px;
+  display: flex;
+  justify-content: space-around;
+`;
+
+const WrapperLeft = styled.div``;
+
+const WrapperRight = styled.div`
+  width: 600px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const ImgCreateButton = styled.button`
+  width: 150px;
+  height: 50px;
+  font-size: 16px;
+  border-radius: 30px;
+  border: none;
+`;
+
+const ImageBox = styled.div`
+  width: 700px;
+  height: 700px;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+`;
+
+const Image = styled.img`
+  width: 200px;
+  height: 200px;
+  object-fit: cover;
+`;
+
+const NoSelected = styled.div`
+  width: 500px;
+  height: 500px;
+  background-color: #bebebe;
+`;
+
+const SelectedImg = styled.img`
+  width: 500px;
+  height: 500px;
+  object-fit: cover;
+  opacity: 70%;
+`;
+
+const PhraseInput = styled.input`
+  width: 500px;
+  height: 50px;
+`;
+
+const Thumnail = styled.div`
+  position: relative;
+`;
+
+const Phrase = styled.div`
+  position: absolute;
+  left: 40%;
+  top: 50%;
+`;
+
+export default function OpenApiPage() {
   const [photos, setPhotos] = useState([]);
+  const [select, setSelect] = useState("");
+  const [phrase, setPhrase] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("https://api.unsplash.com/photos/random", {
+        params: {
+          client_id: "HZktArCXABK1NInMmUDD6JO2_WXQTPifCDCga0Fst1A",
+          count: 9,
+        },
+      })
+      .then((res) => {
+        const result = res.data.map((el) => el.urls.regular);
+        setPhotos(result);
+      });
+  }, []);
+
   const onClickCreateImage = () => {
     axios
       .get("https://api.unsplash.com/photos/random", {
         params: {
-          client_id: "",
+          client_id: "HZktArCXABK1NInMmUDD6JO2_WXQTPifCDCga0Fst1A",
           count: 9,
         },
       })
@@ -20,15 +101,46 @@ export default function OpenapiWithUseEffectPage() {
   };
   console.log(photos);
 
+  const onClickSelectImage = (e) => {
+    setSelect(e);
+  };
+
+  const onChangePhrase = (e) => {
+    setPhrase(e.target.value);
+  };
+
   return (
-    <div>
-      <div>오픈 API 연습</div>
-      <button onClick={onClickCreateImage}>랜덤생성</button>
-      {photos.map((el, idx) => (
-        <>
-          <img key={idx} src={el} />
-        </>
-      ))}
-    </div>
+    <Wrapper>
+      <WrapperLeft>
+        <ImgCreateButton onClick={onClickCreateImage}>
+          랜덤 이미지
+        </ImgCreateButton>
+        <ImageBox>
+          {photos.map((el, idx) => (
+            <>
+              <Image
+                key={idx}
+                src={el}
+                onClick={() => onClickSelectImage(el)}
+              />
+            </>
+          ))}
+        </ImageBox>
+      </WrapperLeft>
+      <WrapperRight>
+        <Thumnail>
+          {select ? (
+            <SelectedImg src={select} />
+          ) : (
+            <NoSelected>이미지를 선택</NoSelected>
+          )}
+          <Phrase>{phrase}</Phrase>
+        </Thumnail>
+        <PhraseInput
+          onChange={onChangePhrase}
+          placeholder="사진에 넣을 문구를 입력하세요"
+        />
+      </WrapperRight>
+    </Wrapper>
   );
 }
