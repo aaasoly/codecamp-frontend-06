223075ce0e995feac1f7141ee6ff8990179446c8
aaasoly <imnamesol@gmail.com>
2022-07-06 20:24:18 +0900

    :art: unsplash api 데코 기능 추가
    unsplash api 데코 기능 추가

--- freeboard_frontend/pages/boards/api/index.tsx
+++ freeboard_frontend/pages/boards/api/index.tsx
@@ -4,20 +4,31 @@ import styled from "@emotion/styled";
 import html2canvas from "html2canvas";
 import domtoimage from "dom-to-image";
 import { saveAs } from "file-saver";
+import { v4 as uuidv4 } from "uuid";
 
 const Wrapper = styled.div`
   width: 1600px;
-  height: 900px;
+  height: 800px;
   display: flex;
   justify-content: space-around;
+  font-size: 16px;
 `;
 
-const WrapperLeft = styled.div``;
+const WrapperLeft = styled.div`
+  width: 700px;
+  height: 800px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
 
 const WrapperRight = styled.div`
   width: 600px;
   display: flex;
   flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 const ImgCreateButton = styled.button`
@@ -45,7 +56,7 @@ const Image = styled.img`
 const NoSelected = styled.div`
   width: 500px;
   height: 500px;
-  background-color: #bebebe;
+  background-color: #e5e5e5;
 `;
 
 const SelectedImg = styled.img`
@@ -53,30 +64,62 @@ const SelectedImg = styled.img`
   height: 500px;
   object-fit: cover;
   opacity: ${(props) => props.opacity}%;
+  filter: ${(props) => (props.isGray ? "grayscale(100%)" : "none")};
 `;
 
-const PhraseInput = styled.input`
+const PhraseInput = styled.textarea`
   width: 500px;
   height: 50px;
+  resize: none;
 `;
 
 export const Thumnail = styled.div`
   position: relative;
-  /* width: 500px;
+  width: 500px;
   height: 500px;
-  background-image: url(${(props) => props.select}); */
 `;
 
-const Phrase = styled.div`
+const Phrase = styled.span`
   position: absolute;
-  left: 40%;
+  left: 50%;
   top: 50%;
+  transform: translate(-50%, -50%);
+  color: ${(props) => (props.isWhite ? "#fff" : "#333")};
+  padding: 10px;
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 12px;
+  max-width: 430px;
+  max-height: 280px;
+  word-wrap: break-word;
 `;
 
 const OpacityBox = styled.div`
   display: flex;
-  width: 200px;
+  width: 250px;
   justify-content: space-around;
+  align-items: center;
+`;
+
+const ImgColorBox = styled.div`
+  display: flex;
+  width: 150px;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+const TextColorBox = styled.div`
+  display: flex;
+  width: 150px;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+const DownloadBtn = styled.button`
+  width: 150px;
+  height: 50px;
+  font-size: 16px;
+  border-radius: 30px;
+  border: none;
 `;
 
 export default function OpenApiPage() {
@@ -84,21 +127,24 @@ export default function OpenApiPage() {
   const [select, setSelect] = useState("");
   const [phrase, setPhrase] = useState("");
   const [opacity, setOpacity] = useState(100);
+  const [isGray, setIsGray] = useState(false);
+  const [isWhite, setIsWhite] = useState(false);
   const canvasRef = useRef(null);
 
-  useEffect(() => {
-    axios
-      .get("https://api.unsplash.com/photos/random", {
-        params: {
-          client_id: "HZktArCXABK1NInMmUDD6JO2_WXQTPifCDCga0Fst1A",
-          count: 9,
-        },
-      })
-      .then((res) => {
-        const result = res.data.map((el) => el.urls.regular);
-        setPhotos(result);
-      });
-  }, []);
+  // 하루 리페치 제한 있어서 꺼두기
+  // useEffect(() => {
+  //   axios
+  //     .get("https://api.unsplash.com/photos/random", {
+  //       params: {
+  //         client_id: "HZktArCXABK1NInMmUDD6JO2_WXQTPifCDCga0Fst1A",
+  //         count: 9,
+  //       },
+  //     })
+  //     .then((res) => {
+  //       const result = res.data.map((el) => el.urls.regular);
+  //       setPhotos(result);
+  //     });
+  // }, []);
 
   const onClickCreateImage = () => {
     axios
@@ -124,6 +170,22 @@ export default function OpenApiPage() {
 
   const onChangeOpacity = (e) => {
     setOpacity(e.target.value);
+  };
+
+  const onChangeBW = () => {
+    setIsGray(true);
+  };
+
+  const onChangeColor = () => {
+    setIsGray(false);
+  };
+
+  const onChangeTextWhite = () => {
+    setIsWhite(true);
+  };
+
+  const onChangeTextBlack = () => {
+    setIsWhite(false);
   };
 
   // const onClickImgSave = (e) => {
@@ -159,37 +221,38 @@ export default function OpenApiPage() {
   return (
     <Wrapper>
       <WrapperLeft>
-        <ImgCreateButton onClick={onClickCreateImage}>
-          랜덤 이미지
-        </ImgCreateButton>
         <ImageBox>
-          {photos.map((el, idx) => (
+          {photos.map((el) => (
             <>
               <Image
-                key={idx}
+                key={uuidv4()}
                 src={el}
                 onClick={() => onClickSelectImage(el)}
               />
             </>
           ))}
         </ImageBox>
+        <ImgCreateButton onClick={onClickCreateImage}>
+          랜덤 이미지
+        </ImgCreateButton>
       </WrapperLeft>
       <WrapperRight>
         <Thumnail ref={canvasRef}>
-          {/* <SelectedImg src={select} opacity={opacity} /> */}
           {select ? (
-            <SelectedImg id="capture" src={select} opacity={opacity} />
+            <SelectedImg src={select} opacity={opacity} isGray={isGray} />
           ) : (
             <NoSelected>이미지를 선택</NoSelected>
           )}
-          {/* /<SelectedImg src="/img/1.jpg" /> */}
-          <Phrase>{phrase}</Phrase>
+
+          <Phrase isWhite={isWhite}>{phrase}</Phrase>
         </Thumnail>
         <PhraseInput
           onChange={onChangePhrase}
+          maxLength={400}
           placeholder="사진에 넣을 문구를 입력하세요"
         />
         <OpacityBox>
+          Opacity:
           <input
             type="radio"
             name="opacity"
@@ -219,7 +282,21 @@ export default function OpenApiPage() {
           />
           100
         </OpacityBox>
-        <button onClick={onDownloadBtn}>이미지 저장</button>
+        <ImgColorBox>
+          색상:
+          <input type="radio" name="color" onChange={onChangeBW} />
+          흑백
+          <input type="radio" name="color" onChange={onChangeColor} />
+          컬러
+        </ImgColorBox>
+        <TextColorBox>
+          글자색:
+          <input type="radio" name="textColor" onChange={onChangeTextBlack} />
+          검정
+          <input type="radio" name="textColor" onChange={onChangeTextWhite} />
+          흰색
+        </TextColorBox>
+        <DownloadBtn onClick={onDownloadBtn}>이미지 저장</DownloadBtn>
       </WrapperRight>
     </Wrapper>
   );
